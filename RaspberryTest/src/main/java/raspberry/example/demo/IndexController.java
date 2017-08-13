eba9f0197715fd4f98de4c20f0c5b20e2e391459
teliseeva <tatiana,zuyeva@gmail.com>
2017-08-13 23:50:42 +0300

    Added pi4j lib, reading data from DTH11

--- RaspberryTest/src/main/java/raspberry/example/demo/IndexController.java
+++ RaspberryTest/src/main/java/raspberry/example/demo/IndexController.java
@@ -4,18 +4,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexController {
-	
-	public static class TemHumSensor{
-		public long Temperature;
-		public long Humidity;
-	}
-	
-	@RequestMapping("/tempHum")
+public class IndexController {	
+	int DTH11pin = 14;
+		
+	 @RequestMapping("/tempHum")
 	 public TemHumSensor GetTemperature() {
-		 TemHumSensor data = new TemHumSensor();
-		 data.Temperature = Math.round(20 + Math.random()*10);
-		 data.Humidity = Math.round(50 + Math.random()*10);
+		 DHT11Manager sensor = new DHT11Manager();
+		 TemHumSensor data = sensor.getTemperature(DTH11pin);
 		 return data;
 	 }	
 }
---
+++ RaspberryTest/src/main/java/raspberry/example/demo/TemHumSensor.java
@@ -0,0 +1,7 @@
+package raspberry.example.demo;
+
+public class TemHumSensor{
+	public float Temperature;
+	public float Humidity;
+}
+
