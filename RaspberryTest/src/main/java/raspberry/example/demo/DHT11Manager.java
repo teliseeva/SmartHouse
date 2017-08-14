b7240dee1a2c8301e8ebc5c91deef285c9c7dce3
teliseeva <tatiana,zuyeva@gmail.com>
2017-08-14 23:53:13 +0300

    fixed pin number

--- RaspberryTest/src/main/java/raspberry/example/demo/DHT11Manager.java
+++ RaspberryTest/src/main/java/raspberry/example/demo/DHT11Manager.java
@@ -1,4 +1,6 @@
 package raspberry.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.pi4j.wiringpi.Gpio;
@@ -6,96 +8,100 @@ import com.pi4j.wiringpi.GpioUtil;
 
 @Service
 public class DHT11Manager {
-	    private static final int    MAXTIMINGS  = 85;
-	    private final int[]         dht11_dat   = { 0, 0, 0, 0, 0 };
+	
+	@Value("{dht11.pin}")
+	private int pin;
 
-	    public DHT11Manager() {
+	private static final int MAXTIMINGS = 85;
+	private final int[] dht11_dat = { 0, 0, 0, 0, 0 };
 
-	        // setup wiringPi
-	        if (Gpio.wiringPiSetup() == -1) {
-	            System.out.println(" ==>> GPIO SETUP FAILED");
-	            return;
-	        }
+	public DHT11Manager() {
 
-	        GpioUtil.export(3, GpioUtil.DIRECTION_OUT);
-	    }
-	    
-	    public TemHumSensor getTemHum() {
-	    	
-	    	for(int i = 0; i < 100; i++) {
-	    		float[] th = getTemperature(14);
-				if(th != null) {
-					TemHumSensor sensor = new TemHumSensor();
-	    			sensor.setHumidity(th[0]);
-	    			sensor.setTemperature(th[1]);
-	    			return sensor;
-	    		}
-	    	}
-	    	return new TemHumSensor();
-	    }
+		// setup wiringPi
+		if (Gpio.wiringPiSetup() == -1) {
+			System.out.println(" ==>> GPIO SETUP FAILED");
+			return;
+		}
 
-	    private float[] getTemperature(final int pin) {
-	    	
-	        int laststate = Gpio.HIGH;
-	        int j = 0;
-	        dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
+		GpioUtil.export(3, GpioUtil.DIRECTION_OUT);
+	}
 
-	        Gpio.pinMode(pin, Gpio.OUTPUT);
-	        Gpio.digitalWrite(pin, Gpio.LOW);
-	        Gpio.delay(18);
+	public TemHumSensor getTemHum() {
 
-	        Gpio.digitalWrite(pin, Gpio.HIGH);
-	        Gpio.pinMode(pin, Gpio.INPUT);
+		for (int i = 0; i < 100; i++) {
+			float[] th = getTemperature(pin);
+			if (th != null) {
+				TemHumSensor sensor = new TemHumSensor();
+				sensor.setHumidity(th[0]);
+				sensor.setTemperature(th[1]);
+				return sensor;
+			}
+		}
+		return new TemHumSensor();
+	}
 
-	        for (int i = 0; i < MAXTIMINGS; i++) {
-	            int counter = 0;
-	            while (Gpio.digitalRead(pin) == laststate) {
-	                counter++;
-	                Gpio.delayMicroseconds(1);
-	                if (counter == 255) {
-	                    break;
-	                }
-	            }
+	private float[] getTemperature(final int pin) {
 
-	            laststate = Gpio.digitalRead(pin);
+		int laststate = Gpio.HIGH;
+		int j = 0;
+		dht11_dat[0] = dht11_dat[1] = dht11_dat[2] = dht11_dat[3] = dht11_dat[4] = 0;
 
-	            if (counter == 255) {
-	                break;
-	            }
+		Gpio.pinMode(pin, Gpio.OUTPUT);
+		Gpio.digitalWrite(pin, Gpio.LOW);
+		Gpio.delay(18);
 
-	            /* ignore first 3 transitions */
-	            if (i >= 4 && i % 2 == 0) {
-	                /* shove each bit into the storage bytes */
-	                dht11_dat[j / 8] <<= 1;
-	                if (counter > 16) {
-	                    dht11_dat[j / 8] |= 1;
-	                }
-	                j++;
-	            }
-	        }
-	        // check we read 40 bits (8bit x 5 ) + verify checksum in the last
-	        // byte
-	        if (j >= 40 && checkParity()) {
-	            float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
-	            if (h > 100) {
-	                h = dht11_dat[0]; // for DHT11
-	            }
-	            float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
-	            if (c > 125) {
-	                c = dht11_dat[2]; // for DHT11
-	            }
-	            if ((dht11_dat[2] & 0x80) != 0) {
-	                c = -c;
-	            }
-	            float[] res = new float[2];
-	            res[0] = h;
-	            res[1] = c;
-	        }
-	        return null;
-	    }
+		Gpio.digitalWrite(pin, Gpio.HIGH);
+		Gpio.pinMode(pin, Gpio.INPUT);
 
-	    private boolean checkParity() {
-	        return dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3] & 0xFF);
-	    }
+		for (int i = 0; i < MAXTIMINGS; i++) {
+			int counter = 0;
+			while (Gpio.digitalRead(pin) == laststate) {
+				counter++;
+				Gpio.delayMicroseconds(1);
+				if (counter == 255) {
+					break;
+				}
+			}
+
+			laststate = Gpio.digitalRead(pin);
+
+			if (counter == 255) {
+				break;
+			}
+
+			/* ignore first 3 transitions */
+			if (i >= 4 && i % 2 == 0) {
+				/* shove each bit into the storage bytes */
+				dht11_dat[j / 8] <<= 1;
+				if (counter > 16) {
+					dht11_dat[j / 8] |= 1;
+				}
+				j++;
+			}
+		}
+		// check we read 40 bits (8bit x 5 ) + verify checksum in the last
+		// byte
+		if (j >= 40 && checkParity()) {
+			float h = (float) ((dht11_dat[0] << 8) + dht11_dat[1]) / 10;
+			if (h > 100) {
+				h = dht11_dat[0]; // for DHT11
+			}
+			float c = (float) (((dht11_dat[2] & 0x7F) << 8) + dht11_dat[3]) / 10;
+			if (c > 125) {
+				c = dht11_dat[2]; // for DHT11
+			}
+			if ((dht11_dat[2] & 0x80) != 0) {
+				c = -c;
+			}
+			float[] res = new float[2];
+			res[0] = h;
+			res[1] = c;
+		}
+		return null;
+	}
+
+	private boolean checkParity() {
+		return dht11_dat[4] == (dht11_dat[0] + dht11_dat[1] + dht11_dat[2] + dht11_dat[3] & 0xFF);
+	}
 
 }
